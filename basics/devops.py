817b9934fbdda4ed529766027e5d5c527d2ccda8
mohamedanas22 <mohamedanas22@gmail.com>
2024-03-26 16:16:16 +0530

    updated

---
+++ basics/devops.py
@@ -0,0 +1,67 @@
+# dict={"name":"anas","age":24}
+# print(dict["name"].index())
+# strs="missisipi"
+# print(strs.rfind("s"))
+# str="elepant"
+# print("index of",str,"is",str.index("e"))
+# str="enviroment pollution"
+# print(str.rstrip("pollution"))
+# count
+
+# a=25
+# b=45
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+# def calculate(operation, num1, num2):
+#     if operation == 'add':
+#          return num1 + num2
+#     elif operation == 'subtract':
+#          return num1 - num2
+#     elif operation == 'multiply':
+#          return num1 * num2
+#     elif operation == 'divide':
+#          return num1 / num2
+#     elif operation ==  'floor_division':
+#          return num1 // num2
+#     elif operation ==  'modulus':
+#         return num1 % num2
+#     else:
+#         return "Invalid operation"
+
+# operation = input("Enter the operation (add, subtract, multiply, divide,floor_division,modulus): ").lower()
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+
+# shopping_cart = {
+#     "mobile_phone": "1",
+#     "ear_phone": "3",
+#     "case_cover": "1",
+#     "accessories": "6"
+# }
+
+# price = {
+#     "mobile_phone": "19000",
+#     "ear_phone": "3500",
+#     "case_cover": "350",
+#     "accessories": "220"
+# }
+
+# total_amount = 0
+# for item, quantity in shopping_cart.items():
+   
+#     total_amount += int(quantity) * int(price[item])
+
+# print(f"The total amount of cart items is: {total_amount}")
+
+
+# for a in range(7):
+#     if a==3:
+#         pass
+#     else:
+#         print(a)
---
+++ basics/module/main.py
@@ -0,0 +1,33 @@
+import text as tp
+
+while True:
+    text = input("Enter a piece of text: ")
+
+    print("\nChoose an option:")
+    print("1. Calculate the number of words in the text.")
+    print("2. Calculate the number of characters in the text.")
+    print("3. Reverse the text.")
+    print("4. Capitalize the text.")
+    print("5. Exit the program.")
+
+    choice = input("Enter your choice (1/2/3/4/5): ")
+
+    if choice == '1':
+        print(f"Number of words: {tp.count_words(text)}")
+    elif choice == '2':
+        print(f"Number of characters (excluding spaces): {tp.count_characters(text)}")
+    elif choice == '3':
+        print(f"Reversed text: {tp.reverse_text(text)}")
+    elif choice == '4':
+        print(f"Capitalized text: {tp.capitalize_text(text)}")
+    elif choice == '5':
+        print("Exiting the program.")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
+    continue_choice = input("Do you want to perform another operation? (y/n): ").lower()
+    if continue_choice != 'y':
+        print("Exiting the program.")
+        break
+
